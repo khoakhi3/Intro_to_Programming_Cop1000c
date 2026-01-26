5c2c673c3328a22a813a05d59230be5c7a3ed6e0
Khoa Duong <khoakhi3@gmail.com>
2026-01-25 21:09:09 -0500

    created a calculator

---
+++ calculator.py
@@ -0,0 +1,43 @@
+#Calculation for multiply.
+num1 = float(input("Enter first number to multiply: "))
+num2 = float(input("Enter second number to multiply: "))
+answer = num1 * num2
+print("The answer to your query is", answer)
+print()
+
+#Division
+num1 = float(input("Enter dividend: "))
+num2 = float(input("Enter divisor: "))
+answer = num1 / num2
+print("The answer to your query is", answer)
+print()
+
+#Division without remainder.
+num1 = int(input("Enter dividend: "))
+num2 = int(input("Enter divisor: "))
+answer = num1 // num2
+print("The answer to your query is", answer)
+print()
+
+#division with remainder only.
+num1 = int(input("Enter dividend: "))
+num2 = int(input("Enter divisor: "))
+answer = num1 % num2
+print("The answer to your query is", answer)
+print()
+
+#Addition
+num1 = int(input("Enter first number to add: "))
+num2 = int(input("Enter second number to add: "))
+answer = num1 + num2
+print("The answer to your query is", answer)
+print()
+
+#Subtraction
+num1 = int(input("Enter number to subtract from: "))
+num2 = int(input("Enter number to subtract: "))
+answer = num1 - num2
+print("The answer to your query is", answer)
+print()
+
+print("Khoa Duong")
